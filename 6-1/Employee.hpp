3793f76b3b8bc69efdd449c200ec767fd5e7c5e7
jinho9610 <jinho9610@naver.com>
2021-11-03 14:44:20 +0900

    사원 관리 시스템 시작

---
+++ 6-1/Employee.hpp
@@ -0,0 +1,39 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <string.h>
+
+#include <iostream>
+
+using namespace std;
+
+class Employee {
+   private:
+    string name;  // 이름
+    int age;      // 나이
+
+    string position;  // 직책
+    int rank;         // 직급
+
+   public:
+    Employee(string name, int age, string position, int rank) : name(name), age(age), position(position), rank(rank) {}  // 초기화 리스트 사용한느 생성자
+
+    Employee(const Employee& employee) {
+        name = employee.name;
+        age = employee.age;
+        position = employee.position;
+        rank = employee.rank;
+    }
+
+    Employee() {}  // default
+
+    void print_info() {
+        cout << name << " (" << position << ", " << age << ") -->" << calculate_pay() << "$" << endl;
+    }
+
+    int calculate_pay() const {
+        return 200 + rank * 50;
+    }
+};
+
+#endif
---
+++ 6-1/EmployeeList.hpp
@@ -0,0 +1,43 @@
+#ifndef EMPLOYEELIST_H
+#define EMPLOYEELIST_H
+
+#include "Employee.hpp"
+
+class EmployeeList {
+   private:
+    int alloc_employee;        // 할당 가능한 최대 직원 수
+    int current_employee;      // 현재 직원 수
+    Employee** employee_list;  // Employee 객체의 주소를 저장하는 배열
+
+   public:
+    EmployeeList(int alloc_employee) : alloc_employee(alloc_employee) {
+        employee_list = new Employee*[alloc_employee];
+        current_employee = 0;  // 최초 직원 수 0명
+    }
+    ~EmployeeList() {
+        for (int i = 0; i < current_employee; ++i) {
+            delete employee_list[i];
+        }
+        delete[] employee_list;
+    }
+
+    void add_employee(Employee* employee) {
+        // 최대 할당 가능 직원수보다 현재 직원수가 늘 작다고 가정
+        employee_list[current_employee++] = employee;
+    }
+
+    int current_employee_num() const {
+        return current_employee;
+    }
+
+    void print_employee_info() {
+        int total_pay = 0;
+        for (int i = 0; i < current_employee; ++i) {
+            total_pay += employee_list[i]->calculate_pay();
+        }
+
+        cout << "총 인건비 : " << total_pay << endl;
+    }
+};
+
+#endif
