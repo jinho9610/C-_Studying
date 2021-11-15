7436566324637026da992e3e46ffd1fdb772f811
jinho9610 <jinho9610@naver.com>
2021-11-15 16:11:21 +0900

    mutex를 이용한 cout << 중단 방지

--- 15-1/main.cpp
+++ 15-1/main.cpp
@@ -1,20 +1,23 @@
 #include <cstdio>
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
 
 using namespace std;
 
-void worker(vector<int>::iterator start, vector<int>::iterator end, int* result) {
+void worker(vector<int>::iterator start, vector<int>::iterator end, int* result, mutex* m) {
     int sum = 0;
     for (auto it = start; it < end; ++it) {
         sum += *it;
     }
     *result = sum;
 
+    m->lock();
     cout << "쓰레드 ID: " << this_thread::get_id() << " --> "
          << *start << " ~ " << *end << "까지 합 계산: "
          << *result << endl;
+    m->unlock();
 }
 
 int main() {
@@ -25,11 +28,12 @@ int main() {
 
     vector<int> partial_sums(4);  // 2500개 씩 부분 합을 구하여 저장함
 
+    mutex m;
     vector<thread> workers;
     for (int i = 0; i < 4; i++) {
         workers.push_back(
             thread(worker, data.begin() + i * 2500, data.begin() + (i + 1) * 2500,
-                   &partial_sums[i]));
+                   &partial_sums[i], &m));
     }
 
     for (int i = 0; i < 4; i++) {
